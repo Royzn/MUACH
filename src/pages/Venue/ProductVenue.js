a27faa6d4850efd6c227bd79ad47595d26e0c8d3
Reymen Sow <reymensow123@gmail.com>
2022-06-01 20:46:55 +0700

    1 juni 2022

--- src/pages/Venue/ProductVenue.js
+++ src/pages/Venue/ProductVenue.js
@@ -6,8 +6,20 @@ function ProductVenue (props) {
     return (
         <div className="productContVenue">
      
-            <img src={props.imageURL} alt="Venue"/>
+     <div className="smallSq">
+                <p>Indoor</p>
+            </div>
+            <img src={props.imageURL} alt="Product"/>
             <h2>{props.id}</h2>
+            <div className="Desc">
+                <div className="star">
+                    *****
+                </div>
+                <p>
+                    nama <br></br>
+                    Jakarta
+                </p>
+            </div>
       
         </div>
     )
