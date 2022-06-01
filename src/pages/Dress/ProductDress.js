5b1591d4a8df99e1674248ed1b2b457133de5717
Royzn <102381434+Royzn@users.noreply.github.com>
2022-06-01 20:25:41 +0700

    produkEdit (#4)
    * ayam
    
    * roy
    
    * productEdit

--- src/pages/Dress/ProductDress.js
+++ src/pages/Dress/ProductDress.js
@@ -5,10 +5,20 @@ import TheProducts from "./dataDress";
 function ProductDress (props) {
     return (
         <div className="productContDress">
-     
+            <div className="smallSq">
+                <p>Indoor</p>
+            </div>
             <img src={props.imageURL} alt="Product"/>
             <h2>{props.id}</h2>
-      
+            <div className="Desc">
+                <div className="star">
+                    *****
+                </div>
+                <p>
+                    nama <br></br>
+                    Jakarta
+                </p>
+            </div>
         </div>
     )
 }
